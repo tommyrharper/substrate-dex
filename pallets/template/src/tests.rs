e8ea29332eaf7ff07ece141efa58c98d703533a2
tommyrharper <thomasrobertharper@gmail.com>
2022-07-31 17:50:51 +0100

    refactor: add tests mod

--- pallets/template/src/tests.rs
+++ pallets/template/src/tests.rs
@@ -4,105 +4,134 @@ use frame_support::{
 	traits::{tokens::fungibles::Mutate, Currency},
 };
 
-const USER: AccountId = 1u32;
-const ASSET1: u32 = 1u32;
-const ASSET2: u32 = 2u32;
-const ASSET1_AMOUNT: u128 = 1;
-const ASSET2_AMOUNT: u128 = 1;
-const MINTED_AMOUNT: u128 = 1_000_000_000;
+#[cfg(test)]
+mod tests {
+	use crate::tests::*;
 
-fn give_user_asset(user: AccountId, asset: u32, amount: u128) {
-    let origin = Origin::signed(user);
-    Balances::make_free_balance_be(&user, amount);
-    Assets::create(origin, asset, user, 1).expect("Asset creation failed");
-    Assets::mint_into(asset, &user, amount).expect("Minting failed");
-}
+	const USER: AccountId = 1u32;
+	const ASSET1: u32 = 1u32;
+	const ASSET2: u32 = 2u32;
+	const ASSET1_AMOUNT: u128 = 1;
+	const ASSET2_AMOUNT: u128 = 1;
+	const MINTED_AMOUNT: u128 = 1_000_000_000;
 
-fn give_user_two_assets(user: AccountId, asset1: u32, asset2: u32, amount: u128) {
-    give_user_asset(user, asset1, amount);
-    give_user_asset(user, asset2, amount);
-}
+	fn give_user_asset(user: AccountId, asset: u32, amount: u128) {
+		let origin = Origin::signed(user);
+		Balances::make_free_balance_be(&user, amount);
+		Assets::create(origin, asset, user, 1).expect("Asset creation failed");
+		Assets::mint_into(asset, &user, amount).expect("Minting failed");
+	}
 
-#[test]
-fn it_works_for_default_value() {
-	new_test_ext().execute_with(|| {
-		// Dispatch a signed extrinsic.
-		assert_ok!(TemplateModule::do_something(Origin::signed(1), 42));
-		// Read pallet storage and assert an expected result.
-		assert_eq!(TemplateModule::something(), Some(42));
-	});
-}
+	fn give_user_two_assets(user: AccountId, asset1: u32, asset2: u32, amount: u128) {
+		give_user_asset(user, asset1, amount);
+		give_user_asset(user, asset2, amount);
+	}
 
-#[test]
-fn correct_error_for_none_value() {
-	new_test_ext().execute_with(|| {
-		// Ensure the expected error is thrown when no value is present.
-		assert_noop!(TemplateModule::cause_error(Origin::signed(1)), Error::<Test>::NoneValue);
-	});
-}
+	#[test]
+	fn it_works_for_default_value() {
+		new_test_ext().execute_with(|| {
+			// Dispatch a signed extrinsic.
+			assert_ok!(TemplateModule::do_something(Origin::signed(1), 42));
+			// Read pallet storage and assert an expected result.
+			assert_eq!(TemplateModule::something(), Some(42));
+		});
+	}
 
-#[test]
-fn provide_liquidity_without_any_tokens() {
-	new_test_ext().execute_with(|| {
-		let origin = Origin::signed(USER);
-		assert_noop!(
-			TemplateModule::provide_liquidity(origin, ASSET1, ASSET2, ASSET1_AMOUNT, ASSET2_AMOUNT),
-			Error::<Test>::NotEnoughTokensToStake
-		);
-	});
-}
+	#[test]
+	fn correct_error_for_none_value() {
+		new_test_ext().execute_with(|| {
+			// Ensure the expected error is thrown when no value is present.
+			assert_noop!(TemplateModule::cause_error(Origin::signed(1)), Error::<Test>::NoneValue);
+		});
+	}
 
-#[test]
-fn provide_liquidity_without_first_token() {
-	new_test_ext().execute_with(|| {
-		give_user_asset(USER, ASSET2, MINTED_AMOUNT);
+	#[test]
+	fn provide_liquidity_without_any_tokens() {
+		new_test_ext().execute_with(|| {
+			let origin = Origin::signed(USER);
+			assert_noop!(
+				TemplateModule::provide_liquidity(
+					origin,
+					ASSET1,
+					ASSET2,
+					ASSET1_AMOUNT,
+					ASSET2_AMOUNT
+				),
+				Error::<Test>::NotEnoughTokensToStake
+			);
+		});
+	}
 
-		let origin = Origin::signed(USER);
-		assert_noop!(
-			TemplateModule::provide_liquidity(origin, ASSET1, ASSET2, ASSET1_AMOUNT, ASSET2_AMOUNT),
-			Error::<Test>::NotEnoughTokensToStake
-		);
-	});
-}
+	#[test]
+	fn provide_liquidity_without_first_token() {
+		new_test_ext().execute_with(|| {
+			give_user_asset(USER, ASSET2, MINTED_AMOUNT);
 
-#[test]
-fn provide_liquidity_without_second_token() {
-	new_test_ext().execute_with(|| {
-		give_user_asset(USER, ASSET1, MINTED_AMOUNT);
+			let origin = Origin::signed(USER);
+			assert_noop!(
+				TemplateModule::provide_liquidity(
+					origin,
+					ASSET1,
+					ASSET2,
+					ASSET1_AMOUNT,
+					ASSET2_AMOUNT
+				),
+				Error::<Test>::NotEnoughTokensToStake
+			);
+		});
+	}
 
-		let origin = Origin::signed(USER);
-		assert_noop!(
-			TemplateModule::provide_liquidity(origin, ASSET1, ASSET2, ASSET1_AMOUNT, ASSET2_AMOUNT),
-			Error::<Test>::NotEnoughTokensToStake
-		);
-	});
-}
+	#[test]
+	fn provide_liquidity_without_second_token() {
+		new_test_ext().execute_with(|| {
+			give_user_asset(USER, ASSET1, MINTED_AMOUNT);
 
-#[test]
-fn provide_liquidity() {
-	new_test_ext().execute_with(|| {
-        give_user_two_assets(USER, ASSET1, ASSET2, MINTED_AMOUNT);
+			let origin = Origin::signed(USER);
+			assert_noop!(
+				TemplateModule::provide_liquidity(
+					origin,
+					ASSET1,
+					ASSET2,
+					ASSET1_AMOUNT,
+					ASSET2_AMOUNT
+				),
+				Error::<Test>::NotEnoughTokensToStake
+			);
+		});
+	}
 
-		let origin = Origin::signed(USER);
-		assert_ok!(TemplateModule::provide_liquidity(
-			origin,
-			ASSET1,
-			ASSET2,
-			ASSET1_AMOUNT,
-			ASSET2_AMOUNT,
-		),);
-	});
-}
+	#[test]
+	fn provide_liquidity() {
+		new_test_ext().execute_with(|| {
+			give_user_two_assets(USER, ASSET1, ASSET2, MINTED_AMOUNT);
 
-#[test]
-fn provide_liquidity_same_asset_ids() {
-	new_test_ext().execute_with(|| {
-        give_user_two_assets(USER, ASSET1, ASSET2, MINTED_AMOUNT);
+			let origin = Origin::signed(USER);
+			assert_ok!(TemplateModule::provide_liquidity(
+				origin,
+				ASSET1,
+				ASSET2,
+				ASSET1_AMOUNT,
+				ASSET2_AMOUNT,
+			),);
+		});
+	}
 
-		let origin = Origin::signed(USER);
-		assert_noop!(
-			TemplateModule::provide_liquidity(origin, ASSET1, ASSET1, ASSET1_AMOUNT, ASSET2_AMOUNT),
-			Error::<Test>::ProvidedInvalidAssetIds
-		);
-	});
+	#[test]
+	fn provide_liquidity_same_asset_ids() {
+		new_test_ext().execute_with(|| {
+			give_user_two_assets(USER, ASSET1, ASSET2, MINTED_AMOUNT);
+
+			let origin = Origin::signed(USER);
+			assert_noop!(
+				TemplateModule::provide_liquidity(
+					origin,
+					ASSET1,
+					ASSET1,
+					ASSET1_AMOUNT,
+					ASSET2_AMOUNT
+				),
+				Error::<Test>::ProvidedInvalidAssetIds
+			);
+		});
+	}
 }
