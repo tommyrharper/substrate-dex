d31ba429e3477b8a14e6729007f8a50e1b8c119a
tommyrharper <thomasrobertharper@gmail.com>
2022-08-02 20:44:40 +0100

    refactor: asset1 to asset_b

--- pallets/template/src/lib.rs
+++ pallets/template/src/lib.rs
@@ -89,42 +89,27 @@ pub mod pallet {
 	// Errors inform users that something went wrong.
 	#[pallet::error]
 	pub enum Error<T> {
-		/// Error names should be descriptive.
-		NoneValue,
-		/// Errors should have helpful documentation associated with them.
-		StorageOverflow,
 		// The user tried to stake more tokens than they have
 		NotEnoughTokensToStake,
 		// The user did not provide valid asset ids
 		ProvidedInvalidAssetIds,
 	}
 
-	// Dispatchable functions allows users to interact with the pallet and invoke state changes.
-	// These functions materialize as "extrinsics", which are often compared to transactions.
-	// Dispatchable functions must be annotated with a weight and must return a DispatchResult.
 	#[pallet::call]
 	impl<T: Config> Pallet<T>
 	where
 		<T::MultiAssets as Inspect<T::AccountId>>::AssetId: AtLeast32Bit,
 		<T::MultiAssets as Inspect<T::AccountId>>::AssetId: Codec,
-		/* where <T::MultiAssets
-		 * as Inspect<T::AccountId>>::AssetId:
-		 * AtLeast32Bit,
-		 * <T::MultiAssets as
-		 * Inspect<T::AccountId>>::AssetId:
-		 * Display */
 	{
-		// Args AssetsPallet::Config::AssetId,
-		// DispatchResult {
 		// TODO: see if tuples are a practical input here
-		// TODO: update asset1 and 2 to a and b
+		// TODO: update asset_a and 2 to a and b
 		#[pallet::weight(10_000 + T::DbWeight::get().reads_writes(1,1))]
 		pub fn create_pool(
 			origin: OriginFor<T>,
-			asset1: AssetIdOf<T>,
-			asset2: AssetIdOf<T>,
-			asset1_amount: BalanceOf<T>,
-			asset2_amount: BalanceOf<T>,
+			asset_a: AssetIdOf<T>,
+			asset_b: AssetIdOf<T>,
+			asset_a_amount: BalanceOf<T>,
+			asset_b_amount: BalanceOf<T>,
 		) -> DispatchResult {
 			// check if message is signed
 			let sender = ensure_signed(origin)?;
@@ -132,12 +117,12 @@ pub mod pallet {
 			// Check the user input is valid
 			Self::check_create_pool_input_is_valid(
 				&sender,
-				(asset1, asset2),
-				(asset1_amount, asset2_amount),
+				(asset_a, asset_b),
+				(asset_a_amount, asset_b_amount),
 			)?;
 
 			// Create the new liquidity pool
-			Self::create_new_pool(&sender, (asset1, asset2), (asset1_amount, asset2_amount))?;
+			Self::create_new_pool(&sender, (asset_a, asset_b), (asset_a_amount, asset_b_amount))?;
 
 			Ok(())
 		}
@@ -145,64 +130,27 @@ pub mod pallet {
 		#[pallet::weight(5_000 + T::DbWeight::get().reads_writes(1,1))]
 		pub fn provide_liquidity(
 			origin: OriginFor<T>,
-			asset1: AssetIdOf<T>,
-			asset2: AssetIdOf<T>,
-			asset1_amount: BalanceOf<T>,
+			asset_a: AssetIdOf<T>,
+			asset_b: AssetIdOf<T>,
+			asset_a_amount: BalanceOf<T>,
 		) -> DispatchResult {
 			// check if message is signed
 			let sender = ensure_signed(origin)?;
 
-			let asset2_amount = Self::derive_second_asset_amount((asset1, asset2), asset1_amount)?;
+			let asset_b_amount = Self::derive_second_asset_amount((asset_a, asset_b), asset_a_amount)?;
 
 			// Check the user input is valid
 			// TODO: update to check ratios are correct
 			// Self::check_create_pool_input_is_valid(
 			//     &sender,
-			// 	(asset1, asset2),
-			// 	(asset1_amount, asset2_amount),
+			// 	(asset_a, asset_b),
+			// 	(asset_a_amount, asset_b_amount),
 			// )?;
 
 			// Top up the liquidity pool
-			Self::top_up_liquidity_pool(&sender, (asset1, asset2), (asset1_amount, asset2_amount))?;
+			Self::top_up_liquidity_pool(&sender, (asset_a, asset_b), (asset_a_amount, asset_b_amount))?;
 
 			Ok(())
-		}
-
-		/// An example dispatchable that takes a singles value as a parameter, writes the value to
-		/// storage and emits an event. This function must be dispatched by a signed extrinsic.
-		#[pallet::weight(10_000 + T::DbWeight::get().writes(1))]
-		pub fn do_something(origin: OriginFor<T>, something: u32) -> DispatchResult {
-			// Check that the extrinsic was signed and get the signer.
-			// This function will return an error if the extrinsic is not signed.
-			// https://docs.substrate.io/v3/runtime/origins
-			let who = ensure_signed(origin)?;
-
-			// Update storage.
-			<Something<T>>::put(something);
-
-			// Emit an event.
-			Self::deposit_event(Event::SomethingStored(something, who));
-			// Return a successful DispatchResultWithPostInfo
-			Ok(())
-		}
-
-		/// An example dispatchable that may throw a custom error.
-		#[pallet::weight(10_000 + T::DbWeight::get().reads_writes(1,1))]
-		pub fn cause_error(origin: OriginFor<T>) -> DispatchResult {
-			let _who = ensure_signed(origin)?;
-
-			// Read a value from storage.
-			match <Something<T>>::get() {
-				// Return an error if the value has not been set.
-				None => return Err(Error::<T>::NoneValue.into()),
-				Some(old) => {
-					// Increment the value read from storage; will error in the event of overflow.
-					let new = old.checked_add(1).ok_or(Error::<T>::StorageOverflow)?;
-					// Update the value in storage with the incremented result.
-					<Something<T>>::put(new);
-					Ok(())
-				},
-			}
 		}
 	}
 }
--- pallets/template/src/tests.rs
+++ pallets/template/src/tests.rs
@@ -10,11 +10,10 @@ mod tests {
 
 	const USER: AccountId = 1u32;
 	const USER_2: AccountId = 2u32;
-    // TODO: updated to use ASSET_1 pattern and so on
-	const ASSET1: u32 = 1u32;
-	const ASSET2: u32 = 2u32;
-	const ASSET1_AMOUNT: u128 = 1_000_000;
-	const ASSET2_AMOUNT: u128 = 1_000_000;
+	const ASSET_A: u32 = 1u32;
+	const ASSET_B: u32 = 2u32;
+	const ASSET_A_AMOUNT: u128 = 1_000_000;
+	const ASSET_B_AMOUNT: u128 = 1_000_000;
 	const MINTED_AMOUNT: u128 = 1_000_000_000;
 
 	fn give_user_asset(user: AccountId, asset: u32, amount: u128) {
@@ -73,29 +72,11 @@ mod tests {
 	}
 
 	#[test]
-	fn it_works_for_default_value() {
-		new_test_ext().execute_with(|| {
-			// Dispatch a signed extrinsic.
-			assert_ok!(TemplateModule::do_something(Origin::signed(1), 42));
-			// Read pallet storage and assert an expected result.
-			assert_eq!(TemplateModule::something(), Some(42));
-		});
-	}
-
-	#[test]
-	fn correct_error_for_none_value() {
-		new_test_ext().execute_with(|| {
-			// Ensure the expected error is thrown when no value is present.
-			assert_noop!(TemplateModule::cause_error(Origin::signed(1)), Error::<Test>::NoneValue);
-		});
-	}
-
-	#[test]
 	fn create_pool_without_any_tokens() {
 		new_test_ext().execute_with(|| {
 			let origin = Origin::signed(USER);
 			assert_noop!(
-				TemplateModule::create_pool(origin, ASSET1, ASSET2, ASSET1_AMOUNT, ASSET2_AMOUNT),
+				TemplateModule::create_pool(origin, ASSET_A, ASSET_B, ASSET_A_AMOUNT, ASSET_B_AMOUNT),
 				Error::<Test>::NotEnoughTokensToStake
 			);
 		});
@@ -104,11 +85,11 @@ mod tests {
 	#[test]
 	fn create_pool_without_first_token() {
 		new_test_ext().execute_with(|| {
-			give_user_asset(USER, ASSET2, MINTED_AMOUNT);
+			give_user_asset(USER, ASSET_B, MINTED_AMOUNT);
 
 			let origin = Origin::signed(USER);
 			assert_noop!(
-				TemplateModule::create_pool(origin, ASSET1, ASSET2, ASSET1_AMOUNT, ASSET2_AMOUNT),
+				TemplateModule::create_pool(origin, ASSET_A, ASSET_B, ASSET_A_AMOUNT, ASSET_B_AMOUNT),
 				Error::<Test>::NotEnoughTokensToStake
 			);
 		});
@@ -117,11 +98,11 @@ mod tests {
 	#[test]
 	fn create_pool_without_second_token() {
 		new_test_ext().execute_with(|| {
-			give_user_asset(USER, ASSET1, MINTED_AMOUNT);
+			give_user_asset(USER, ASSET_A, MINTED_AMOUNT);
 
 			let origin = Origin::signed(USER);
 			assert_noop!(
-				TemplateModule::create_pool(origin, ASSET1, ASSET2, ASSET1_AMOUNT, ASSET2_AMOUNT),
+				TemplateModule::create_pool(origin, ASSET_A, ASSET_B, ASSET_A_AMOUNT, ASSET_B_AMOUNT),
 				Error::<Test>::NotEnoughTokensToStake
 			);
 		});
@@ -130,11 +111,11 @@ mod tests {
 	#[test]
 	fn create_pool_same_asset_ids() {
 		new_test_ext().execute_with(|| {
-			give_user_two_assets(USER, ASSET1, ASSET2, MINTED_AMOUNT);
+			give_user_two_assets(USER, ASSET_A, ASSET_B, MINTED_AMOUNT);
 
 			let origin = Origin::signed(USER);
 			assert_noop!(
-				TemplateModule::create_pool(origin, ASSET1, ASSET1, ASSET1_AMOUNT, ASSET2_AMOUNT),
+				TemplateModule::create_pool(origin, ASSET_A, ASSET_A, ASSET_A_AMOUNT, ASSET_B_AMOUNT),
 				Error::<Test>::ProvidedInvalidAssetIds
 			);
 		});
@@ -143,15 +124,15 @@ mod tests {
 	#[test]
 	fn create_pool_with_enough_assets() {
 		new_test_ext().execute_with(|| {
-			give_user_two_assets(USER, ASSET1, ASSET2, MINTED_AMOUNT);
+			give_user_two_assets(USER, ASSET_A, ASSET_B, MINTED_AMOUNT);
 
 			let origin = Origin::signed(USER);
 			assert_ok!(TemplateModule::create_pool(
 				origin,
-				ASSET1,
-				ASSET2,
-				ASSET1_AMOUNT,
-				ASSET2_AMOUNT,
+				ASSET_A,
+				ASSET_B,
+				ASSET_A_AMOUNT,
+				ASSET_B_AMOUNT,
 			),);
 		});
 	}
@@ -159,39 +140,39 @@ mod tests {
 	#[test]
 	fn can_transfer_assets() {
 		new_test_ext().execute_with(|| {
-			give_user_two_assets(USER, ASSET1, ASSET2, MINTED_AMOUNT);
+			give_user_two_assets(USER, ASSET_A, ASSET_B, MINTED_AMOUNT);
 			Balances::make_free_balance_be(&USER_2, ExistentialDeposit::get());
 			let origin = Origin::signed(USER);
-			assert_ok!(Assets::transfer(origin, ASSET1, USER_2, ASSET1_AMOUNT));
+			assert_ok!(Assets::transfer(origin, ASSET_A, USER_2, ASSET_A_AMOUNT));
 		});
 	}
 
 	#[test]
 	fn create_pool_transfers_tokens() {
 		new_test_ext().execute_with(|| {
-			give_user_two_assets(USER, ASSET1, ASSET2, MINTED_AMOUNT);
+			give_user_two_assets(USER, ASSET_A, ASSET_B, MINTED_AMOUNT);
 
 			let origin = Origin::signed(USER);
 
 			assert_ok!(TemplateModule::create_pool(
 				origin,
-				ASSET1,
-				ASSET2,
-				ASSET1_AMOUNT,
-				ASSET2_AMOUNT,
+				ASSET_A,
+				ASSET_B,
+				ASSET_A_AMOUNT,
+				ASSET_B_AMOUNT,
 			),);
 
 			check_liquidity_taken(
 				USER,
-				(ASSET1, ASSET2),
+				(ASSET_A, ASSET_B),
 				(MINTED_AMOUNT, MINTED_AMOUNT),
-				(ASSET1_AMOUNT, ASSET2_AMOUNT),
+				(ASSET_A_AMOUNT, ASSET_B_AMOUNT),
 			);
 
 			check_lp_tokens_sent_to_pool_creator(
 				USER,
-				(ASSET1, ASSET2),
-				(ASSET1_AMOUNT, ASSET2_AMOUNT),
+				(ASSET_A, ASSET_B),
+				(ASSET_A_AMOUNT, ASSET_B_AMOUNT),
 			);
 		});
 	}
@@ -199,40 +180,40 @@ mod tests {
 	#[test]
 	fn provide_liquidity() {
 		new_test_ext().execute_with(|| {
-			give_user_two_assets(USER, ASSET1, ASSET2, MINTED_AMOUNT);
+			give_user_two_assets(USER, ASSET_A, ASSET_B, MINTED_AMOUNT);
 
 			let origin = Origin::signed(USER);
 
 			assert_ok!(TemplateModule::create_pool(
 				origin,
-				ASSET1,
-				ASSET2,
-				ASSET1_AMOUNT,
-				ASSET2_AMOUNT,
+				ASSET_A,
+				ASSET_B,
+				ASSET_A_AMOUNT,
+				ASSET_B_AMOUNT,
 			),);
 
 			let origin = Origin::signed(USER_2);
 
 			assert_ok!(TemplateModule::provide_liquidity(
 				origin,
-				ASSET1,
-				ASSET2,
-				ASSET1_AMOUNT,
+				ASSET_A,
+				ASSET_B,
+				ASSET_A_AMOUNT,
 			),);
 
             check_liquidity_taken(
 				USER_2,
-				(ASSET1, ASSET2),
+				(ASSET_A, ASSET_B),
 				(MINTED_AMOUNT, MINTED_AMOUNT),
-				(ASSET1_AMOUNT, ASSET2_AMOUNT),
+				(ASSET_A_AMOUNT, ASSET_B_AMOUNT),
 			);
 
 			check_lp_tokens_sent_to_provider(
 				USER_2,
-				(ASSET1, ASSET2),
-				ASSET1_AMOUNT,
-                ASSET1_AMOUNT,
-                ASSET1_AMOUNT,
+				(ASSET_A, ASSET_B),
+				ASSET_A_AMOUNT,
+                ASSET_A_AMOUNT,
+                ASSET_A_AMOUNT,
 			);
 		});
 	}
